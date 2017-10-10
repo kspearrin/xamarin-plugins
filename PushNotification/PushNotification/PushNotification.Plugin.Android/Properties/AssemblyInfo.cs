3ff2ba3fe40138522ed023757b8db3cc36af5773
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-10-09 22:00:41 -0400

    remove android manifest permissions

--- PushNotification/PushNotification/PushNotification.Plugin.Android/Properties/AssemblyInfo.cs
+++ PushNotification/PushNotification/PushNotification.Plugin.Android/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using Android.App;
 
 [assembly: AssemblyTitle("PushNotification.Plugin.Android")]
 [assembly: AssemblyDescription("")]
@@ -13,9 +11,3 @@ using Android.App;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: Permission(Name = "@PACKAGE_NAME@.permission.C2D_MESSAGE")]
-[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
-[assembly: UsesPermission(Name = "com.google.android.c2dm.permission.RECEIVE")]
-[assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
-[assembly: UsesPermission(Name = "android.permission.INTERNET")]
